e47c3081a693e01d664341cf7b4c191ce6c9dce8
Diucla <claudiosingular@gmail.com>
2017-11-18 17:33:03 +0200

    Adicionando classes bootstrap aos inputs

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,18 +10,23 @@
 </head>
 <body>
 
-	@if(count($errors) > 0)
+	<div class="jumbotron">
 
-	<ul>
-		@foreach ($errors->all() as $error)
-		<li class="laranja-text">{{ $error }}</li>
-		@endforeach
-	</ul>
+		<h1 class="display-3">Exemplo basico com Laravel</h1>
+		<p class="lead">CRUD</p>
+		<hr class="my-4">
+		@if(count($errors) > 0)
 
-	@endif
+		<ul>
+			@foreach ($errors->all() as $error)
+			<li class = "alert alert-danger" role="alert">{{ $error }}</li>
+			@endforeach
+		</ul>
 
-	@yield('main')
-	
+		@endif
+
+		@yield('main')
+	</div>
 
 	<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
--- resources/views/markets/create.blade.php
+++ resources/views/markets/create.blade.php
@@ -1,19 +1,38 @@
 @extends('layouts.app')
 @section('main')
 
-<form action="{{ route('markets.store') }}" method="post">
+<div class="offset-md-4 col-md-4 card card-default mt-3 p-2">
+	
+	<form action="{{ route('markets.store') }}" method="post">
 
-	{{ csrf_field() }}
+		{{ csrf_field() }}
 
-	<label for="name">Market Name</label>
-	<input type="text" name="name"><br>
-	<label for="city">Market City</label>
-	<input type="text" name="city"><br>
-	<label for="website">Market WebSite</label>
-	<input type="text" name="website"><br>
-	<button type="submit">Create</button>
+		<div class="input-group p-1">
 
-</form>
+			<label class="input-group-addon" for="name">Market Name</label>
+
+			<input class="form-control" type="text" name="name">
+
+		</div>
+
+		<div class="input-group p-1">
+
+			<label class="input-group-addon" for="city">Market City</label>
+			<input class="form-control" type="text" name="city">
+		</div>
+
+		<div class="input-group p-1">
+
+			<label class="input-group-addon" for="website">Market WebSite</label>
+			<input class="form-control" type="text" name="website">
+
+		</div>
+		<button class="btn verde pull-right" type="submit">Create</button>
+
+	</form>
+
+
+</div>
 
 
 @endsection
